1098da070f997e65a24538b58a1e67467651e8c9
Aaron Ang <67321817+aaron-ang@users.noreply.github.com>
2024-05-10 22:05:46 -0400

    Respond to multiple PINGs

--- src/main.rs
+++ src/main.rs
@@ -23,19 +23,23 @@ fn main() {
 
 fn handle_connection(mut stream: TcpStream) {
     let mut buf = [0; 512];
-    match stream.read(&mut buf) {
-        Ok(_n) => {
-            let command = String::from_utf8(buf[0.._n].to_vec());
-            if command.is_ok() {
-                let command = command.unwrap();
-                if command.contains("PING") {
-                    println!("responding with PONG\r\n");
-                    stream.write_all(b"+PONG\r\n").unwrap();
+    loop {
+        match stream.read(&mut buf) {
+            Ok(0) => break,
+            Ok(n) => {
+                let command = String::from_utf8(buf[0..n].to_vec());
+                if command.is_ok() {
+                    let command = command.unwrap();
+                    if command.contains("PING") {
+                        println!("responding with PONG\r\n");
+                        stream.write_all(b"+PONG\r\n").unwrap();
+                    }
                 }
             }
+            Err(e) => {
+                println!("error: {}", e);
+            }
         }
-        Err(e) => {
-            println!("error: {}", e);
-        }
+        buf = [0; 512];
     }
 }

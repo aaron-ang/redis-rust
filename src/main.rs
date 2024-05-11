23fc1da6b127f277e0890eeda21b17e545616d60
Aaron Ang <67321817+aaron-ang@users.noreply.github.com>
2024-05-10 21:59:21 -0400

    PING PONG

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,41 @@
-use std::net::TcpListener;
+use std::{
+    io::{Read, Write},
+    net::{TcpListener, TcpStream},
+};
+
+static PORT: u16 = 6379;
 
 fn main() {
-    println!("Logs from your program will appear here!");
-
-    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
+    let listener = TcpListener::bind(format!("127.0.0.1:{PORT}")).unwrap();
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(stream) => {
                 println!("accepted new connection");
+                handle_connection(stream);
             }
             Err(e) => {
                 println!("error: {}", e);
             }
+        }
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buf = [0; 512];
+    match stream.read(&mut buf) {
+        Ok(_n) => {
+            let command = String::from_utf8(buf[0.._n].to_vec());
+            if command.is_ok() {
+                let command = command.unwrap();
+                if command.contains("PING") {
+                    println!("responding with PONG\r\n");
+                    stream.write_all(b"+PONG\r\n").unwrap();
+                }
+            }
+        }
+        Err(e) => {
+            println!("error: {}", e);
         }
     }
 }

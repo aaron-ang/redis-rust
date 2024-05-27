b883b9528143d6d381fb3ebbe2267a1ff425d78e
Aaron Ang <67321817+aaron-ang@users.noreply.github.com>
2024-05-27 02:17:30 -0400

    parse multiple commands in buf

--- src/server.rs
+++ src/server.rs
@@ -8,13 +8,13 @@ use std::{io::BufReader, sync::Arc, time::SystemTime};
 use tokio::{
     io::{AsyncReadExt, AsyncWriteExt},
     net::TcpStream,
-    sync::broadcast::Sender,
-    time::{sleep, Duration},
+    sync::broadcast::{error::RecvError, Sender},
+    time::Duration,
 };
 
-const REPL_ID: &str = "8371b4fb1155b71f4a04d3e1bc3e18c4a990aeeb";
-const REPL_OFFSET: usize = 0;
-const EMPTY_RDB_B64: &str = "UkVESVMwMDEx+glyZWRpcy12ZXIFNy4yLjD6CnJlZGlzLWJpdHPAQPoFY3RpbWXCbQi8ZfoIdXNlZC1tZW3CsMQQAPoIYW9mLWJhc2XAAP/wbjv+wP9aog==";
+pub const REPL_ID: &str = "8371b4fb1155b71f4a04d3e1bc3e18c4a990aeeb";
+pub const REPL_OFFSET: usize = 0;
+pub const EMPTY_RDB_B64: &str = "UkVESVMwMDEx+glyZWRpcy12ZXIFNy4yLjD6CnJlZGlzLWJpdHPAQPoFY3RpbWXCbQi8ZfoIdXNlZC1tZW3CsMQQAPoIYW9mLWJhc2XAAP/wbjv+wP9aog==";
 
 pub struct Server {
     store: Store,
@@ -28,60 +28,74 @@ impl Server {
 
     pub async fn handle_conn(&self, mut stream: TcpStream, tx: Arc<Sender<Value>>) -> Result<()> {
         let mut replication = false;
-
-        let tx = tx.clone();
+        let mut buf = [0; 1024];
 
         loop {
-            sleep(Duration::from_millis(10)).await;
-
-            let mut buf = [0; 1024];
             let bytes_read = stream.read(&mut buf).await?;
             if bytes_read == 0 {
                 eprintln!("Client disconnected");
                 break;
             }
-            let bufreader = BufReader::new(&buf[..bytes_read]);
-            let mut decoder = Decoder::new(bufreader);
-
-            let response = match decoder.decode() {
-                Ok(value) => {
-                    let (command, args) = extract_command(&value).unwrap();
-                    match command.to_lowercase().as_str() {
-                        "ping" => Some(Value::String("PONG".into())),
-                        "echo" => Some(args.first().unwrap().clone()),
-                        "set" => handle_set(args, &self.store).map(|v| {
-                            tx.send(value.clone()).unwrap();
-                            println!("Sent to followers: {:?}", value);
-                            v
-                        }),
-                        "get" => handle_get(args, &self.store),
-                        "info" => handle_info(self.role),
-                        "replconf" => Some(Value::String("OK".into())),
-                        "psync" => handle_psync(&mut stream, &mut replication).await,
-                        c => {
-                            eprintln!("Unknown command: {}", c);
+            let mut decoder = Decoder::new(BufReader::new(&buf[..bytes_read]));
+            while let Some(value) = decoder.decode().ok() {
+                let (command, args) = extract_command(&value).unwrap();
+                let response = match command.to_lowercase().as_str() {
+                    "ping" => Some(Value::String("PONG".into())),
+                    "echo" => Some(args.first().unwrap().clone()),
+                    "set" => {
+                        let r = handle_set(args, &self.store);
+                        if r.is_some() && self.role == ReplicaType::Leader {
+                            tx.send(value).unwrap();
+                            r
+                        } else {
                             None
                         }
                     }
+                    "get" => handle_get(args, &self.store),
+                    "info" => handle_info(self.role),
+                    "replconf" => Some(Value::String("OK".into())),
+                    "psync" => handle_psync(&mut stream, &mut replication).await,
+                    c => {
+                        eprintln!("Unknown command: {}", c);
+                        None
+                    }
+                };
+                if let Some(r) = response {
+                    stream.write_all(&r.encode()).await?;
                 }
-                Err(e) => {
-                    eprintln!("Error decoding value: {:?}", e);
-                    None
-                }
-            };
-
-            if let Some(r) = response {
-                stream.write_all(&r.encode()).await?;
             }
 
             if replication {
                 let mut rx = tx.subscribe();
-                while let Ok(f) = rx.recv().await {
-                    stream.write_all(&f.encode()).await?;
+                loop {
+                    match rx.recv().await {
+                        Ok(v) => {
+                            stream.write_all(&v.encode()).await?;
+                        }
+                        Err(e) => match e {
+                            RecvError::Closed => {
+                                eprintln!("Channel closed");
+                                break;
+                            }
+                            RecvError::Lagged(n) => {
+                                eprintln!("Lagged by {n} messages");
+                            }
+                        },
+                    }
                 }
             }
         }
         Ok(())
+    }
+}
+
+pub fn extract_command(value: &Value) -> Result<(String, Vec<Value>)> {
+    if let Value::Array(a) = value {
+        let command = unpack_bulk_string(a.first().unwrap())?;
+        let args = a.iter().skip(1).cloned().collect();
+        Ok((command, args))
+    } else {
+        Err(anyhow::anyhow!("Unexpected command format"))
     }
 }
 
@@ -92,17 +106,8 @@ fn unpack_bulk_string(value: &Value) -> Result<String> {
         Err(anyhow::anyhow!("Expected command to be bulk string"))
     }
 }
-fn extract_command(value: &Value) -> Result<(String, Vec<Value>)> {
-    if let Value::Array(a) = value {
-        let command = unpack_bulk_string(a.first().unwrap())?;
-        let args = a.iter().skip(1).cloned().collect();
-        Ok((command, args))
-    } else {
-        Err(anyhow::anyhow!("Unexpected command format"))
-    }
-}
 
-fn handle_set(args: Vec<Value>, store: &Store) -> Option<Value> {
+pub fn handle_set(args: Vec<Value>, store: &Store) -> Option<Value> {
     if args.len() < 2 {
         return None;
     }
@@ -129,15 +134,15 @@ fn handle_set(args: Vec<Value>, store: &Store) -> Option<Value> {
     Some(Value::String("OK".into()))
 }
 
-fn handle_get(args: Vec<Value>, store: &Store) -> Option<Value> {
+pub fn handle_get(args: Vec<Value>, store: &Store) -> Option<Value> {
     if args.len() < 1 {
-        return None;
+        return Some(Value::Null);
     }
     let key = unpack_bulk_string(&args[0]).unwrap();
     store
         .get(&key)
         .map(|v| Value::Bulk(v))
-        .or_else(|| Some(Value::Null))
+        .or(Some(Value::Null))
 }
 
 fn handle_info(role: ReplicaType) -> Option<Value> {
@@ -150,7 +155,7 @@ async fn handle_psync(stream: &mut TcpStream, replication: &mut bool) -> Option<
     let msg = Value::String(format!("FULLRESYNC {REPL_ID} {REPL_OFFSET}"));
     stream.write_all(&msg.encode()).await.unwrap();
 
-    let rdb = BASE64_STANDARD.decode(EMPTY_RDB_B64.as_bytes()).unwrap();
+    let rdb = BASE64_STANDARD.decode(EMPTY_RDB_B64).unwrap();
     stream
         .write(format!("${}\r\n", rdb.len()).as_bytes())
         .await
